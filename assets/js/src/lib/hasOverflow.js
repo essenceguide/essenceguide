b25e3f97bb6204897b51abe462dcf7afb93571d2
Dan Colonna <dan@expandtheroom.com>
2016-05-20 00:46:35 -0400

    updates with footer

--- assets/js/src/lib/hasOverflow.js
+++
@@ -1,7 +0,0 @@
-var $ = require('jquery');
-
-'use strict';
-
-module.exports = function(el) {
-  return $(el).outerHeight(true) == $(el).parent().height() || $(el).height() > $(el).parent().height();
-}
